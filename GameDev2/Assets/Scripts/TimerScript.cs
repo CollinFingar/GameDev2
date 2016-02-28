8f2eebd749073d858188c888329734f1d54e1778
Collin Fingar <fingac@rpi.edu>
2016-02-28 14:31:00 -0500

    Made a timer script
    You know how it is.

---
+++ GameDev2/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerScript : MonoBehaviour {
+
+    public float timerLength = 5;
+    public float timerStart = 0;
+    public float timerEnd = 0;
+    public float updateSpriteTime = 0;
+    public int currentSpriteNum = 0;
+
+    public bool inProgress = false;
+
+	// Use this for initialization
+	void Start () {
+        startTime(10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (inProgress)
+        {
+            updateTimer();
+        }
+	}
+
+    bool startTime(float length)
+    {
+        if (inProgress)
+        {
+            return false;
+        }
+        currentSpriteNum = 0;
+        inProgress = true;
+        timerLength = length;
+        timerStart = Time.time;
+        timerEnd = timerStart + timerLength;
+        updateSpriteTime = Time.time + timerLength / 8;
+        return true;
+    }
+
+    void updateTimer()
+    {
+        float time = Time.time;
+        if(time >= timerEnd)
+        {
+            currentSpriteNum++;
+            updateSprite();
+            inProgress = false;
+        }
+        else
+        {
+            if(time > updateSpriteTime)
+            {
+                currentSpriteNum++;
+                updateSprite();
+                updateSpriteTime += timerLength / 8;
+
+            }
+        }
+    }
+
+    void updateSprite()
+    {
+        Debug.Log("updating sprite: " + currentSpriteNum);
+    }
+}
