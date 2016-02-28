b4e63ff32cda6503494d14f9a482926c84ed6eac
Collin Fingar <fingac@rpi.edu>
2016-02-28 13:11:26 -0500

    Added Color Representation of War
    This is a new object that is tied to the two Faction objects. These faction objects also have variables to keep track of resources, weapons, etc.

---
+++ GameDev2/Assets/Scripts/FactionScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FactionScript : MonoBehaviour {
+
+    public int faction = 0;
+
+    public float money = 0;
+
+    public float infantryPowerValue = 0;
+    public float spacePowerValue = 0;
+    public int[] infantryResources = new int[5];
+    public int[] spaceResources = new int[5];
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ GameDev2/Assets/Scripts/WarScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarScript : MonoBehaviour {
+    public GameObject Faction1;
+    public GameObject Faction2;
+    
+    public float warStatus = .5f;
+
+    public GameObject colors;
+    public float farRightX = 6f;
+    public float farLeftX = -6f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow)){
+            if(warStatus < 1)
+            {
+                warStatus += .01f;
+            }
+        } else if (Input.GetKey(KeyCode.LeftArrow)){
+            if(warStatus > 0)
+            {
+                warStatus -= .01f;
+            }
+        }
+        colors.transform.position = new Vector3(farRightX * warStatus + farLeftX * (1 - warStatus), transform.position.y, transform.position.z);
+	}
+}
