3592f5396714851997ae1d50bfb070c36e6aa408
Collin Fingar <fingac@rpi.edu>
2016-04-13 13:31:40 -0400

    Can you feel it, Mr. Krabs?

--- GameDev2/Assets/Scripts/PlayerScript.cs
+++ GameDev2/Assets/Scripts/PlayerScript.cs
@@ -9,6 +9,11 @@ public class PlayerScript : MonoBehaviour {
     public float resourceFuel = 0;
     public float resourcePlasma = 0;
 
+    public float moneyDisplayed = 0;
+    public float resourceMetalDisplayed = 0;
+    public float resourceFuelDisplayed = 0;
+    public float resourcePlasmaDisplayed = 0;
+
     public Text moneyText;
     public Text AText;
     public Text BText;
@@ -25,7 +30,10 @@ public class PlayerScript : MonoBehaviour {
 
     public int metalIncome = 0;
     public int fuelIncome = 0;
-    public int plasmaIncome = 0; 
+    public int plasmaIncome = 0;
+
+    public int resourceDisplayTick = 1;
+    public int moneyDisplayTick = 10;
 
     // Use this for initialization
     void Start () {
@@ -34,10 +42,12 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        moneyText.text = "$: "+money.ToString();
-        AText.text = "Metal: "+ resourceMetal.ToString();
-        BText.text = "Fuel: "+ resourceFuel.ToString();
-        CText.text = "Plasma: "+ resourcePlasma.ToString();
+        incrementValues();
+
+        moneyText.text = "$: "+ moneyDisplayed.ToString();
+        AText.text = "Metal: "+ resourceMetalDisplayed.ToString();
+        BText.text = "Fuel: "+ resourceFuelDisplayed.ToString();
+        CText.text = "Plasma: "+ resourcePlasmaDisplayed.ToString();
         if (Input.GetKeyDown(KeyCode.Escape)) {
             Application.Quit();
         }
@@ -69,6 +79,72 @@ public class PlayerScript : MonoBehaviour {
         else if (type == "Plasma")
         {
             resourcePlasma += amount;
+        }
+    }
+
+    public void incrementValues() {
+        if (moneyDisplayed != money) {
+            if (moneyDisplayed < money) {
+                if (money - moneyDisplayed >= moneyDisplayTick) {
+                    moneyDisplayed += moneyDisplayTick;
+                } else {
+                    moneyDisplayed += (money - moneyDisplayed);
+                }
+            } else {
+                if (moneyDisplayed - money >= moneyDisplayTick) {
+                    moneyDisplayed -= moneyDisplayTick;
+                } else {
+                    moneyDisplayed -= (moneyDisplayed - money);
+                }
+            }
+        }
+
+        if (resourceMetalDisplayed != resourceMetal) {
+            if (resourceMetalDisplayed < resourceMetal) {
+                if (resourceMetal - resourceMetalDisplayed >= resourceDisplayTick) {
+                    resourceMetalDisplayed += resourceDisplayTick;
+                } else {
+                    resourceMetalDisplayed += (resourceMetal - resourceMetalDisplayed);
+                }
+            } else {
+                if (resourceMetalDisplayed - resourceMetal >= resourceDisplayTick) {
+                    resourceMetalDisplayed -= resourceDisplayTick;
+                } else {
+                    resourceMetalDisplayed -= (resourceMetalDisplayed - resourceMetal);
+                }
+            }
+        }
+
+        if (resourceFuelDisplayed != resourceFuel) {
+            if (resourceFuelDisplayed < resourceFuel) {
+                if (resourceFuel - resourceFuelDisplayed >= resourceDisplayTick) {
+                    resourceFuelDisplayed += resourceDisplayTick;
+                } else {
+                    resourceFuelDisplayed += (resourceFuel - resourceFuelDisplayed);
+                }
+            } else {
+                if (resourceFuelDisplayed - resourceFuel >= resourceDisplayTick) {
+                    resourceFuelDisplayed -= resourceDisplayTick;
+                } else {
+                    resourceFuelDisplayed -= (resourceFuelDisplayed - resourceFuel);
+                }
+            }
+        }
+
+        if (resourcePlasmaDisplayed != resourcePlasma) {
+            if (resourcePlasmaDisplayed < resourcePlasma) {
+                if (resourcePlasma - resourcePlasmaDisplayed >= resourceDisplayTick) {
+                    resourcePlasmaDisplayed += resourceDisplayTick;
+                } else {
+                    resourcePlasmaDisplayed += (resourcePlasma - resourcePlasmaDisplayed);
+                }
+            } else {
+                if (resourcePlasmaDisplayed - resourcePlasma >= resourceDisplayTick) {
+                    resourcePlasmaDisplayed -= resourceDisplayTick;
+                } else {
+                    resourcePlasmaDisplayed -= (resourcePlasmaDisplayed - resourcePlasma);
+                }
+            }
         }
     }
 }
