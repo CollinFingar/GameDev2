f457c93bcba7d5a36570793335992a2c98073e39
charder <chardergaming@gmail.com>
2016-04-19 14:10:13 -0400

    "A few changes to the scene. I don't think much changes from this." - Caleb
    Don't Quote me on that. Oh wait.

--- GameDev2/Assets/Scripts/WarColliderScript.cs
+++ GameDev2/Assets/Scripts/WarColliderScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class WarColliderScript : MonoBehaviour {
 
     public GameObject currentPlanet;
+	public GameObject spaceLasers;
     public string currentPlanetName;
     public bool onPlanet = false;
 
@@ -14,7 +15,17 @@ public class WarColliderScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Vector3 pos1;
+		Vector3 pos2;
+		if (transform.position.x > currentPlanet.transform.position.x) {
+			pos1 = currentPlanet.transform.position;
+			pos2 = currentPlanet.GetComponent<PlanetScript> ().rightNeighbor.transform.position;
+		} 
+		else {
+			pos1 = currentPlanet.GetComponent<PlanetScript> ().leftNeighbor.transform.position;
+			pos2 = currentPlanet.transform.position;
+		}
+		spaceLasers.transform.position = new Vector3 (transform.position.x, Mathf.Lerp (pos1.y, pos2.y, (transform.position.x - pos1.y) / (pos2.y - pos1.y)),-1.0f);
 	}
 
     void OnTriggerEnter2D(Collider2D coll) {
