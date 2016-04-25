48b63885a4096f7bf6d8899027d7b0e177e48104
Collin Fingar <fingac@rpi.edu>
2016-04-25 16:10:30 -0400

    Name Change Scene

---
+++ GameDev2/Assets/Scripts/MainMenu/WriteNamesScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class WriteNamesScript : MonoBehaviour {
+
+    public TextAsset text;
+
+    public int sceneNum = 2;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(text.text);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartTheShow()
+    {
+        SceneManager.LoadScene(sceneNum);
+    }
+}
