3f793bdcb5ea5d699614ec8d5257da21000b977a
Collin Fingar <fingac@rpi.edu>
2016-04-25 16:50:44 -0400

    Name Changing now works
    Praise be!!!!

--- GameDev2/Assets/Scripts/MainMenu/WriteNamesScript.cs
+++ GameDev2/Assets/Scripts/MainMenu/WriteNamesScript.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.IO;
+using UnityEngine.UI;
 
 public class WriteNamesScript : MonoBehaviour {
 
     public TextAsset text;
+
+	public InputField if1;
+	public InputField if2;
 
     public int sceneNum = 2;
 
@@ -20,6 +25,17 @@ public class WriteNamesScript : MonoBehaviour {
 
     public void StartTheShow()
     {
+
+
+
+		StreamWriter  writer = new StreamWriter("Assets/Resources/Names.txt"); // Does this work?
+		writer.Write("");
+
+		writer.WriteLine(if1.text);
+		writer.WriteLine(if2.text);
+		writer.Close ();
+
+
         SceneManager.LoadScene(sceneNum);
     }
 }
