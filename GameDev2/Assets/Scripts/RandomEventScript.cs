9fcc21b7fdec1148adc9e1bf97a219c71135d560
Mark Stein <markstein820@gmail.com>
2016-04-26 13:26:32 -0400

    Started Random Events
    Small Scene Change

---
+++ GameDev2/Assets/Scripts/RandomEventScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct Event{
+	public string title;
+	public string body;
+	public delegate int del(GameObject obj1, GameObject obj2);
+}
+
+public class RandomEventScript : MonoBehaviour {
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	
+}
