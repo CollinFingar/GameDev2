fcc79317c4dab3c11a5e6b7b555377f695db641c
charder <chardergaming@gmail.com>
2016-04-26 15:35:52 -0400

    Instructions ARE clear now.
    Though someone will still screw it up... (Some people amiright?)

---
+++ GameDev2/Assets/Scripts/InstructionScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InstructionScript : MonoBehaviour {
+
+	int cur_index;
+	public Sprite[] instructions; 
+	public Image myImage; //this object's image
+
+	// Use this for initialization
+	void Start () {
+		cur_index = 0; //sets the first instruction image to be visible at star
+		myImage.sprite = instructions[cur_index];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void nextImage() {
+		if (cur_index < (instructions.Length - 1)) 
+		{
+			cur_index++;
+		} 
+		else 
+		{
+			cur_index = 0;
+		}
+		myImage.sprite = instructions [cur_index];		
+	}
+}
