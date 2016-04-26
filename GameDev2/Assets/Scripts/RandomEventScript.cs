732b94344eb058bb9364c9f304f2acf77600e678
Mark Stein <markstein820@gmail.com>
2016-04-26 15:21:34 -0400

    Implemented First Actual Event

--- GameDev2/Assets/Scripts/RandomEventScript.cs
+++ GameDev2/Assets/Scripts/RandomEventScript.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 using UnityEngine.UI;
 using System.IO;
+using Random=UnityEngine.Random;
 
 public struct Event{
 	public string title;
 	public string body;
-	public int eventNum;
 }
 
 public class RandomEventScript : MonoBehaviour {
@@ -15,17 +15,21 @@ public class RandomEventScript : MonoBehaviour {
 	public Text body;
 	
 	public GameObject F1;
+	public Text F1name;
 	public GameObject F2;
+	public Text F2name;
+	public int Faction;
 	
-	public Event E;
+	public Event[] events = new Event[1];
+	public Event E1;
 	
 	// Use this for initialization
 	void Start () {
 		
-		E.title = "Title";
-		E.body = "Body body body body body";
-		E.eventNum = 0;
-	
+		E1.title = "Call to Arms!";
+		E1.body = " conducts a large propaganda campaign to recruit a large force of new soldiers.";
+		
+		events[0] = E1;
 	}
 	
 	// Update is called once per frame
@@ -34,16 +38,26 @@ public class RandomEventScript : MonoBehaviour {
 	}
 	
 	public void initEvent(int num){
-		if(num == E.eventNum){
-			title.text = E.title;
-			body.text = E.body;
+		Faction = Random.Range(0,2);
+		title.text = events[num].title;
+		
+		if(Faction == 0){
+			body.text = F1name.text+events[num].body;
+		}else{
+			body.text = F1name.text+events[num].body;
 		}
+		
+		
 	}
 	
 	public void doEvent(int num){
 		
-		if(num == E.eventNum){
-			F1.GetComponent<FactionScript> ().supplies[0].quantity += 1;
+		if(num == 0){
+			if(Faction == 0){
+				F1.GetComponent<FactionScript> ().supplies[2].quantity += 1;
+			}else{
+				F2.GetComponent<FactionScript> ().supplies[2].quantity += 1;
+			}
 		}
 		
 	}
