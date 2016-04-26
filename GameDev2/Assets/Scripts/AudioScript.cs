8d8d71b3db6dc19c45270bed7c4db739bf51c9ab
charder <chardergaming@gmail.com>
2016-04-26 16:56:23 -0400

    Menu audio is now continuous. Let it flow, let it flow!
    Stop.

---
+++ GameDev2/Assets/Scripts/AudioScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake() {
+		if (GameObject.Find("Audio Source").gameObject != this.gameObject) {
+			Destroy (this.gameObject);
+			return;
+		}
+		DontDestroyOnLoad (this.gameObject);
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameObject.Find ("War") != null) {
+			Destroy (this.gameObject);
+			return;
+		}
+	}
+}
