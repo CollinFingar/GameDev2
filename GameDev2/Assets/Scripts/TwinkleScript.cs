a477d93394e67e921fc6a65a51cf661ceefdbe34
Collin Fingar <fingac@rpi.edu>
2016-04-27 10:37:50 -0400

    Twinkling stars!!!!!
    What a gift, and it's not even Christmas!!

---
+++ GameDev2/Assets/Scripts/TwinkleScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TwinkleScript : MonoBehaviour {
+
+	private float nextTwinkle = 0;
+
+	private SpriteRenderer sr;
+
+	private bool twinkling = false;
+	private bool twinklingDown = false;
+	private float alph = 1f;
+	public float rate = .1f;
+
+	// Use this for initialization
+	void Start () {
+		sr = GetComponent<SpriteRenderer> ();
+		nextTwinkle = Time.time + Random.Range (0, 8);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Time.time);
+		if (!twinkling && Time.time >= nextTwinkle) {
+			twinkling = true;
+			twinklingDown = true;
+		} else if (twinkling) {
+			if (twinklingDown) {
+				if (alph <= .05) {
+					alph = 0;
+
+					twinklingDown = false;
+				} else {
+					alph -= rate;
+				}
+			} else {
+				if (alph >= .95) {
+					alph = 1;
+					twinkling = false;
+					nextTwinkle = Time.time + Random.Range (2, 10);
+				} else {
+					alph += rate;
+				}
+			}
+			sr.color = new Color(1,1,1,alph);
+		}
+	}
+}
