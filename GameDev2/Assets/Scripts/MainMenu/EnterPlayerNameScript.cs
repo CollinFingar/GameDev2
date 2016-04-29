1fa4cce9ee5c13a14d2317e74d8ecf8e89b28528
Collin Fingar <fingac@rpi.edu>
2016-04-29 12:29:57 -0400

    Started Making End Screens

---
+++ GameDev2/Assets/Scripts/MainMenu/EnterPlayerNameScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.IO;
+using UnityEngine.UI;
+
+public class EnterPlayerNameScript : MonoBehaviour {
+	public TextAsset text;
+
+	public InputField if1;
+
+	public int sceneNum = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartTheShow()
+	{
+
+		StreamWriter  writer = new StreamWriter("Assets/Resources/LatestScore.txt", true);
+
+		writer.WriteLine(if1.text);
+		writer.Close ();
+
+
+		SceneManager.LoadScene(sceneNum);
+	}
+
+
+}
